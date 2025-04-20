196f7d0ae247ce4b453e0c0ebf31f6b41670de2a
Sammy2383 <sameerpanday@mail.weber.edu>
2025-04-20 00:23:27 -0600

    Update animal.py

--- animal.py
+++ animal.py
@@ -2,16 +2,35 @@ class Animal:
     """
     Base class representing a generic animal.
     """
-    # Class-level attribute
+    # Class-level attribute for all Animal objects
+    all_animals = []
     kingdom = "Animalia"
-    # TODO create a class-level attribute that is a list of all the Animal objects
 
-    # TODO create the initializer for Animal with name and species as attributs
+    # Initializer with name and species attributes
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        # Add the newly created animal to the all_animals list
+        Animal.all_animals.append(self)
 
-    # TODO: Add a method to make a generic sound 
-    # Call the method `speak` and make it output a specific message like 
-    # "The animal makes a noise.""
+    # Method to make a generic sound
+    def speak(self):
+        print("The animal makes a noise.")
 
-    # TODO __str__ method for string representation
-    # Example output
-    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
+    # String representation method
+    def __str__(self):
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}'"
+
+    @classmethod
+    def from_full_name(cls, name, species):
+        return cls(name[0], species)
+
+    def print_animal_data(self):
+        print(f"ANIMAL INFO \n"
+              f"\tNAME: {self.name}\n"
+              f"\tSPECIES: {self.species}\n"
+              f"\tKINGDOM: {self.kingdom}\n")
+
+    @classmethod
+    def get_all_animals(cls):
+        return cls.all_animals
